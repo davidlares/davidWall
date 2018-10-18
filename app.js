ba0c6b52035ba162e6ba80264ba044588f1557a0
David Lares <david.e.lares@gmail.com>
2018-10-18 19:14:33 -0400

    Configuring services and rending a hello world with pug

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
+const FacebookStrategy = require('passport-strategy').Strategy;
+const graph = require('fbgraph');
+
 const app = express();
 const port = 8000
 
+// bodyParser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+// cookie Session
+app.use(cookieSession({keys: ['mysecretkey', 'yekttercesyek'] }))
+// passport
+app.use(passport.initialize());
+app.use(passport.session())
+// view engine
+app.set('view engine','pug')
+
 app.get('/', (req,res) => {
-  res.end('Hello world');
+  res.render('index');
 });
 
 app.listen(port)
